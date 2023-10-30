7ac200a857489de2c12561faa56d7d8140092fe1
andrewdu1 <andrew.du.87@gmail.com>
2023-10-30 15:27:11 -0600

    Update 2_Fit model to Paranthropus data.R
    Changed EM to MLE

--- Code/2_Fit model to Paranthropus data.R
+++ Code/2_Fit model to Paranthropus data.R
@@ -23,11 +23,11 @@ mle.res <- ZI.BB.mle(
   lambda.start = 100,
   x = x,
   n = n,
-  hessian = TRUE)
+  hessian = FALSE)
 
 # Get out estimated parameters
-psi_hat <- mle.res$param["psi"]
-lambda_hat <- mle.res$param["lambda"]
+psi_hat <- mle.res["psi"]
+lambda_hat <- mle.res["lambda"]
 
 # Calculate posterior probabilities 
 # (prob. Paranthropus is truly absent given estimated parameters and data)
@@ -50,63 +50,69 @@ post_prob.res1 <- data.frame(
 
 #############################################
 
-# bootstrap 95% CIs for hyperparameters
-set.seed(99)
+# bootstrap 95% CIs for hyperparameters & posterior probabilities
+set.seed(99) # for repeatability purposes
 
-n.iter <- 1000 # number of bootstrap iterations
+boot.res <- boot_data( # bootstrap the data
+  x = x,
+  n = n,
+  n.iter = 1000,
+  silent = FALSE
+)
 
-psi_boot <- lambda_boot <- numeric(n.iter) # create empty vectors to save bootstrapped values to
-
-for(i in seq_along(psi_boot)){
+mle.boot.res <- sapply(boot.res, function(i){ # estimate hyperparameters for each bootstrapped iteration
   
-  index <- sample.int(
-    length(x), 
-    size = length(x), 
-    replace = TRUE) # randomly choose index by which to resample sites
-  
-  x_boot <- x[index] # get resampled x
-  n_boot <- n[index] # get resampled n
-  
-  x_boot_spec <- numeric(length(x)) # empty vector to save bootstrapped specimens to
-  
-  for(j in seq_along(x_boot_spec)) x_boot_spec[j] <- 
-    rbinom(
-      n = 1, 
-      size = n_boot[j], 
-      prob = x_boot[j] / n_boot[j]) # for each bootstrapped x and n, resample x (number of specimens)
-  
-  mle.boot <- ZI.BB.mle(
+  ZI.BB.mle(
     psi.start = 0.5,
     lambda.start = 100,
-    x = x_boot_spec,
-    n = n_boot,
-    hessian = FALSE
-  ) # use ML to estimate hyperparameters with bootstrapped x and n
+    x = i$x,
+    n = i$n,
+    hessian = FALSE)
+})
+
+post_prob.boot.res <- apply(mle.boot.res, 2, function(i){ # calculate posterior probability for each bootstrapped iteration
   
-  # save results to empty vectors
-  psi_boot[i] <- mle.boot["psi"]
-  lambda_boot[i] <- mle.boot["lambda"]
-  
-  print(i)
-}
+  post_prob(d1$n, i["psi"], i["lambda"])
+})
 
 # 95% CIs
-quantile(psi_boot, c(0.025, 0.975)) # 0.28-0.67
-quantile(lambda_boot, c(0.025, 0.975)) # 64, 361
+quantile(mle.boot.res["psi", ], c(0.025, 0.975)) # psi: 0.28-0.67
+quantile(mle.boot.res["lambda", ], c(0.025, 0.975)) # lambda: 64-361
+apply(post_prob.boot.res, 1, function(x) quantile(x, c(0.025, 0.975)))
 
 
 #############################################
 
-# Calculated expected frequency of Paranthropus specimens across sites using estimated model parameters
-BBpdf_expect <- lapply(n, function(n1) stdReflectBBpdf(seq(0, n1), n1, lambda_hat)) # calculate probabilities for sampling number of Paranthropus specimens from zero all the way to total number of mammalian specimens at site. Do this using the beta-binomial PMF.
+# Calculate expected frequency distribution of Paranthropus specimens across sites using estimated model parameters
+BBpmf_expect <- array( # empty array to save results to
+  data = 0,
+  dim = c(nrow(d), max(n) + 1) # + 1 includes zero NISP
+)
 
-BBpdf_expect <- lapply(BBpdf_expect, function(x) c(x, rep(0, (max(n) + 1) - length(x)))) # add zeros to the end of each vector for NISP that exceeds the total found at each site
+for(i in seq_len(nrow(BBpmf_expect))){
+  
+  BBpmf_expect[i, seq(1, n[i] + 1)] <- BBpmf(
+    x = seq(0, n[i]), 
+    n = n[i], 
+    lambda = lambda_hat)
+} # calculate probabilities for sampling number of Paranthropus specimens from zero all the way to total number of mammalian specimens at each site. 
 
-BBpdf_expect_sum <- Reduce("+", BBpdf_expect) # sum PMF vectors across sites
+BBpmf_expect_sum <- colSums(BBpmf_expect) # sum PMF vectors across sites
 
-x_expect <- BBpdf_expect_sum * psi_hat # multiply summed probabilities by probability that Paranthropus is at site (psi_hat)
+x_expect <- BBpmf_expect_sum * psi_hat # multiply summed probabilities by probability that Paranthropus is at site (psi_hat)
 
-x_expect[1] <- x_expect[1] + (1 - psi_hat) * length(BBpdf_expect) # add (1 - psi_hat) times number of sites to the expected number of sites with zero Paranthropus. (1 - psi_hat) is the probability Paranthropus is not at the site.
+x_expect[1] <- x_expect[1] + (1 - psi_hat) * nrow(d) # add (1 - psi_hat) times number of sites to the expected number of sites with zero Paranthropus.
 
-# save Paranthropus expected frequency distribution results
-#saveRDS(x_expect, file = "Results/Expected freq dist Paran NISP.rds")
+
+#############################################
+
+# Save results
+res <- list(
+  estim.param = c(psi_hat, lambda_hat),
+  post_prob.res = post_prob.res1,
+  mle.boot = mle.boot.res,
+  post_prob.boot = post_prob.boot.res,
+  x_expect = x_expect
+)
+
+#saveRDS(res, "Results/All model fitting results.rds")
