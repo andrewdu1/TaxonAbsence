c534d4de9e5dee1b0eb12ad25e7e27bf2db73314
Du <andrew.du.87@gmail.com>
2023-11-01 13:59:07 -0600

    Update 1_R functions.R
    Collated all functions here, including those used during model selection

--- Code/1_R functions.R
+++ Code/1_R functions.R
@@ -136,8 +136,10 @@ boot_data <- function(x, n, n.iter = 1000, silent = FALSE){
     # n_sites: number of sites
     # n_mammSpec: vector of number of total mammalian specimens from each site
     # psi: psi value
-    # lambda: lambda value
-simulateData <- function(n_sites, n_mammSpec, psi, lambda){
+    # beta_param: a vector for parameters of beta distribution. 
+      # If length(beta_param) = 1, parameter is lambda.
+      # If length(beta_param) = 2, parameters are alpha & beta of the beta distribution
+simulateData <- function(n_sites, n_mammSpec, psi, beta_param){
   
   require(rmutil) # for simulating random draws from a beta-binomial distribution
   
@@ -147,12 +149,23 @@ simulateData <- function(n_sites, n_mammSpec, psi, lambda){
   
   X <- Z # save to a new object, X, which is the number of recovered Paranthropus specimens at site i
   
+  # define parameterization of beta-binomial
+  if(length(beta_param) == 1){ # one-parameter model
+    s <- 2 + beta_param
+    m <- 1 / s
+  }
+  
+  if(length(beta_param) == 2){ # two-parameter model
+    s <- sum(beta_param)
+    m <- beta_param[1] / s
+  }
+  
   if(sum(Z_equals_1) > 0){
     X[Z_equals_1] <- rbetabinom(
       n = sum(Z_equals_1), 
       size = n_mammSpec[Z_equals_1], 
-      m = 1 / (2 + lambda), 
-      s = 2 + lambda) # for those sites where Z_i = 1, use a beta-binomial to randomly select a Paranthropus abundance value
+      m = m, 
+      s = s) # for those sites where Z_i = 1, use a beta-binomial to randomly select a Paranthropus abundance value
   }
   
   res <- data.frame(
@@ -161,4 +174,208 @@ simulateData <- function(n_sites, n_mammSpec, psi, lambda){
     Z_i = Z) # collate the Paranthropus abundance, total mammal abundance, and Z vectors
   
   return(res)  
+}
+
+
+
+########## functions for other models and model selection ########## 
+# Binomial model log-likelihood
+  # ARGUMENTS:
+    # x: vector of number of successes (# Paranthropus specimens)
+    # n: vector of number of trials (# total mammalian specimens)
+    # p: probability of success parameter
+binom.logL <- function(x, n, p){
+  
+  return(sum(dbinom(x, n, p, log = TRUE)))
+}
+
+# fit binomial model via MLE
+  # ARGUMENTS:
+    # x: vector of number of successes (# Paranthropus specimens)
+    # n: vector of number of trials (# total mammalian specimens)
+    # p.start: initial guess of p to start from
+binom.mle <- function(x, n, p.start){
+  return(optim(par = p.start, 
+               fn = binom.logL,
+               method = "L-BFGS-B",
+               x = x,
+               n = n,
+               lower = 1e-6,
+               upper = 1 - 1e-6,
+               control = list(
+                fnscale = -1
+               )))
+}
+
+
+# one-parameter beta-binomial model log-likelihood
+  # ARGUMENTS:
+    # x: vector of number of successes (# Paranthropus specimens)
+    # n: vector of number of trials (# total mammalian specimens)
+    # lambda: single parameter of beta-binomial
+BB.logL <- function(x, n, lambda){
+  
+  require(rmutil)
+  
+  m <- 1 / (2 + lambda)
+  s <- 2 + lambda
+  
+  return(sum(dbetabinom(x, n, m, s, log = TRUE)))
+}
+
+# fit one-parameter beta-binomial model via MLE
+  # ARGUMENTS:
+    # x: vector of number of successes (# Paranthropus specimens)
+    # n: vector of number of trials (# total mammalian specimens)
+    # lambda.start: initial guess of lambda to start from
+BB.mle <- function(x, n, lambda.start){
+  return(optim(par = lambda.start, 
+               fn = BB.logL,
+               method = "L-BFGS-B",
+               x = x,
+               n = n,
+               lower = 0,
+               upper = 1e6,
+               control = list(
+                 fnscale = -1
+               )))
+}
+
+
+# zero-inflated binomial model log-likelihood
+  # ARGUMENTS:
+    # x: vector of number of successes (# Paranthropus specimens)
+    # n: vector of number of trials (# total mammalian specimens) 
+    # param: two-element vector c(psi, p)
+ZI.binom.logL <- function(x, n, param){
+  
+  psi <- param[1]
+  p <- param[2]
+  
+  return(sum(log((x == 0) * (1 - psi) + dbinom(x, n, p) * psi)))
+}
+
+# fit zero-inflated binomial model via MLE
+# NB: added in the option of changing optim() parameter search bounds 
+# b/c some sites have very low observed relative abundances. Otherwise,
+# optim() fails to converge
+  # ARGUMENTS:
+    # x: vector of number of successes (# Paranthropus specimens)
+    # n: vector of number of trials (# total mammalian specimens)
+    # psi.start: initial guess for psi
+    # p.start: initial guess for p
+    # psi.bounds: bounds for searching for psi values
+    # p.bounds: bounds for searching for p values
+ZI.binom.mle <- function(x, n, psi.start, p.start, psi.bounds, p.bounds){
+  
+  return(optim(par = c(psi.start, p.start), 
+               fn = ZI.binom.logL,
+               method = "L-BFGS-B",
+               x = x,
+               n = n,
+               lower = c(psi.bounds[1], p.bounds[1]),
+               upper = c(psi.bounds[2], p.bounds[2]), 
+               control = list(
+                 fnscale = -1,
+                 ndeps = c(1e-4, 1e-4) # smaller step sizes for search
+               )))
+}
+
+
+# Zero-inflated beta-binomial model (two parameters) log-likelihood
+  # ARGUMENTS:
+    # x: vector of number of successes (# Paranthropus specimens)
+    # n: vector of number of trials (# total mammalian specimens)
+    # param: two-element vector c(psi, lambda)
+ZI.BB.logL <- function(x, n, param){
+  
+  psi <- param[1]
+  lambda <- param[2]
+  
+  require(rmutil)
+  
+  m <- 1 / (2 + lambda)
+  s <- 2 + lambda
+  
+  return(sum(log((x == 0) * (1 - psi) + dbetabinom(x, n, m, s) * psi)))
+}
+
+# Fit zero-inflated beta-binomial model (two parameters) via MLE
+  # ARGUMENTS:
+    # x: vector of number of successes (# Paranthropus specimens)
+    # n: vector of number of trials (# total mammalian specimens)
+    # psi.start: initial guess for psi
+    # lambda.start: initial guess for lambda
+ZI.BB.mle <- function(x, n, psi.start, lambda.start){
+  
+  return(optim(par = c(psi.start, lambda.start), 
+               fn = ZI.BB.logL,
+               method = "L-BFGS-B",
+               x = x,
+               n = n,
+               lower = c(1e-6, 0),
+               upper = c(1, 1e6),
+               control = list(
+                 fnscale = -1
+               )))
+}
+
+
+# Zero-inflated beta-binomial model (three parameters) log-likelihood
+  # ARGUMENTS:
+    # x: vector of number of successes (# Paranthropus specimens)
+    # n: vector of number of trials (# total mammalian specimens)
+    # param: three-element vector c(psi, alpha, beta)
+ZI.BB2.logL <- function(x, n, param){
+  
+  psi <- param[1]
+  m <- param[2] / (param[2] + param[3])
+  s <- param[2] + param[3]
+  
+  require(rmutil)
+  
+  return(sum(log((x == 0) * (1 - psi) + dbetabinom(x, n, m, s) * psi)))
+}
+
+# Fit zero-inflated beta-binomial model (three parameters) via MLE
+  # ARGUMENTS:
+    # x: vector of number of successes (# Paranthropus specimens)
+    # n: vector of number of trials (# total mammalian specimens)
+    # psi.start: initial guess for psi
+    # alpha.start: initial guess for alpha
+    # beta.start: initial guess for beta
+ZI.BB2.mle <- function(x, n, psi.start, alpha.start, beta.start){
+  
+  return(optim(par = c(psi.start, alpha.start, beta.start), 
+               fn = ZI.BB2.logL,
+               method = "L-BFGS-B",
+               x = x,
+               n = n,
+               lower = c(1e-6, 1e-6, 1e-6),
+               upper = c(1, 1e10, 1e10),
+               control = list(
+                 fnscale = -1
+               )))
+}
+
+
+# function for calculating AICc (Burnham & Anderson 2002, pg 324)
+  # ARGUMENTS:
+    # logL: log-likelihood value
+    # k: number of parameters
+    # n: sample size
+AICc <- function(logL, k, n){
+  
+  return(-2 * logL + 2 * k + 2 * k * (k + 1) / (n - k - 1))
+}
+
+
+# function for calculating weights from AICc scores (Burnham & Anderson 2002, pg 75)
+  # ARGUMENTS:
+    # AICc.res: vector of AICc calculations from different models
+AICc_weights <- function(AICc.res){
+  
+  AICc.delta <- AICc.res - min(AICc.res)
+  
+  return(exp(-0.5 * AICc.delta) / sum(exp(-0.5 * AICc.delta)))
 }
