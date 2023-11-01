83d547e481c3d2ea08943171d69f5a514c7d2a80
Du <andrew.du.87@gmail.com>
2023-11-01 14:13:59 -0600

    Update 5_Model selection.R
    Updated after moving functions to main functions R script

--- Code/5_Model selection.R
+++ Code/5_Model selection.R
@@ -4,19 +4,20 @@
 #                         #
 ###########################
 
-# Author: Andrew Du & Eric Friedlander
+# Author: Andrew Du
 
-# This script uses AICc to assess our model's
-# fit against simpler models
+# This script uses AICc to assess our model's fit (ZI.BB) against other models
 
+
+# source functions
+source("Code/1_R functions.R")
 
 # Read in data
 d <- read.csv("Datasets/NISP data.csv")
 x <- d$Paran_nisp # Paranthropus NISP
 n <- d$Paran_nisp + d$NonParanMamm_nisp # mammalian NISP
 
-
-# create empty list to save model results to
+# create empty list to save model fitting results to
 mle.res <- vector(
   mode = "list", 
   length = 5)
@@ -30,189 +31,40 @@ names(mle.res) <- c(
   "ZI.BB2"
 )
 
-
-# Create & fit models
-
-## Binomial model
-binom.logL <- function(x, n, p){
-  
-  return(sum(dbinom(x, n, p, log = TRUE)))
-}
-
-### use optim() to numerically estimate parameter
-binom.fit <- optim(par = 0.5, 
-             fn = binom.logL,
-             method = "L-BFGS-B",
-             x = x,
-             n = n,
-             lower = 1e-6,
-             upper = 1 - 1e-6,
-             control = list(
-               fnscale = -1
-             ))
-
-### save logL & k (# parameters)
-mle.res$binom <- c(
-  binom.fit$value,
-  length(binom.fit$par)
-  )
-
-
-## Beta-binomial model
-BB.logL <- function(x, n, lambda){
-  
-  require(rmutil)
-  
-  m <- 1 / (2 + lambda)
-  s <- 2 + lambda
-
-  return(sum(dbetabinom(x, n, m, s, log = TRUE)))
-}
-
-### use optim() to numerically estimate parameter
-BB.fit <- optim(par = 10, 
-                fn = BB.logL,
-                method = "L-BFGS-B",
-                x = x,
-                n = n,
-                lower = 0,
-                upper = Inf,
-                control = list(
-                  fnscale = -1
-                ))
-
-### save logL & k (# parameters)
-mle.res$BB <- c(
-  BB.fit$value,
-  length(BB.fit$par)
+# Fit models
+mle.res$binom <- binom.mle(x, n, p.start = 0.5)
+mle.res$BB <- BB.mle(x, n, lambda.start = 100)
+mle.res$ZI.binom <- ZI.binom.mle(
+  x,
+  n,
+  psi.start = 0.5,
+  p.start = 0.001,
+  psi.bounds = c(1e-6, 1),
+  p.bounds = c(1e-4, 0.02)
+)
+mle.res$ZI.BB <- ZI.BB.mle(
+  x, 
+  n, 
+  psi.start = 0.5, 
+  lambda.start = 100
+)
+mle.res$ZI.BB2 <- ZI.BB2.mle(
+  x,
+  n,
+  psi.start = 0.5,
+  alpha.start = 5,
+  beta.start = 5
 )
 
+# NB: ZI.BB2 gave nonsensical parameter estimates (i.e., psi_hat = 1),
+# so we excluded this model from the model selection procedure, and 
+# we explored its behavior via simulations
+mle.res1 <- mle.res[names(mle.res) != "ZI.BB2"]
 
-## Zero-inflated binomial model
-  # Argument param is a vector with two elements
-    # First element is psi
-    # Second element is p
-ZI.binom.logL <- function(x, n, param){
-  
-  psi <- param[1]
-  p <- param[2]
-  
-  return(sum(log((x == 0) * (1 - psi) + dbinom(x, n, p) * psi)))
-}
-
-### use optim() to numerically estimate parameters
-ZI.binom.fit <- optim(par = c(0.5, 0.001), 
-                      fn = ZI.binom.logL,
-                      method = "L-BFGS-B",
-                      x = x,
-                      n = n,
-                      lower = c(1e-6, 1e-4),
-                      upper = c(1, 0.02),
-                      control = list(
-                        fnscale = -1
-                      ))
-
-### save logL & k (# parameters)
-mle.res$ZI.binom <- c(
-  ZI.binom.fit$value,
-  length(ZI.binom.fit$par)
-)
-
-
-## Zero-inflated beta-binomial model (one parameter)
-  # Argument param is a vector with two elements
-    # First element is psi
-    # Second element is lambda
-ZI.BB.logL <- function(x, n, param){
-  
-  psi <- param[1]
-  lambda <- param[2]
-  
-  require(rmutil)
-  
-  m <- 1 / (2 + lambda)
-  s <- 2 + lambda
-  
-  return(sum(log((x == 0) * (1 - psi) + dbetabinom(x, n, m, s) * psi)))
-}
-
-### use optim() to numerically estimate parameters
-ZI.BB.fit <- optim(par = c(0.5, 100), 
-                   fn = ZI.BB.logL,
-                   method = "L-BFGS-B",
-                   x = x,
-                   n = n,
-                   lower = c(1e-6, 0),
-                   upper = c(1, Inf),
-                   control = list(
-                     fnscale = -1
-                   ))
-
-### save logL & k (# parameters)
-mle.res$ZI.BB <- c(
-  ZI.BB.fit$value,
-  length(ZI.BB.fit$par)
-)
-
-
-## Zero-inflated beta-binomial model (two parameters)
-  # Argument param is a vector with three elements
-    # First element is psi
-    # Second & third elements are beta-binomial parameters (alpha & beta)
-ZI.BB2.logL <- function(x, n, param){
-  
-  psi <- param[1]
-  m <- param[2] / (param[2] + param[3])
-  s <- param[2] + param[3]
-  
-  require(rmutil)
-  
-  return(sum(log((x == 0) * (1 - psi) + dbetabinom(x, n, m, s) * psi)))
-}
-
-### use optim() to numerically estimate parameters
-ZI.BB2.fit <- optim(par = c(0.5, 1, 1), 
-                   fn = ZI.BB2.logL,
-                   method = "L-BFGS-B",
-                   x = x,
-                   n = n,
-                   lower = c(1e-6, 1e-6, 1e-6),
-                   upper = c(1, 1e10, 1e10),
-                   control = list(
-                     fnscale = -1
-                   ))
-
-### save logL & k (# parameters)
-mle.res$ZI.BB2 <- c(
-  ZI.BB2.fit$value,
-  length(ZI.BB2.fit$par)
-)
-
-
-# Write functions for AICc & AICc weights
-## AICc (Burnham & Anderson 2002, pg 324)
-AICc <- function(logL, k, n){
-  
-  return(-2 * logL + 2 * k + 2 * k * (k + 1) / (n - k - 1))
-}
-
-## AICc weights
-## w_i (Burnham & Anderson 2002, pg 75)
-AICc_weights <- function(AICc.res){
-  
-  AICc.delta <- AICc.res - min(AICc.res)
-  
-  return(exp(-0.5 * AICc.delta) / sum(exp(-0.5 * AICc.delta)))
-}
-
-
-# run functions on model results
-AICc.res <- sapply(mle.res, function(x){
-  
-  AICc(
-    logL = x[1], 
-    k = x[2], 
-    n = nrow(d))
+# Calculate AICc
+AICc.res <- sapply(mle.res1, function(x){
+  AICc(logL = x$value, k = length(x$par), n = nrow(d))
 })
 
+# Calculate AICc weights
 AICc_weights(AICc.res)
